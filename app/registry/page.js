545acc09994c4610491ef5869b32e36d6552ecb7
samlimby <129619263+samlimby@users.noreply.github.com>
2024-08-13 17:42:54 +0100

    Registry Page

--- app/registry/page.js
+++ app/registry/page.js
@@ -1,8 +1,183 @@
+"use client";
+
+import Header from '../components/Header.jsx'; 
+import './registry.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Registry() {
     return (
       <div>
-        <h1>Registry</h1>
-        <p>This is the Registry page placeholder.</p>
+        <Header />
+        <div className="party-body">
+            <div className="grid-block">
+                <div className="member-card">
+                    <div className="upper-card_row">
+                        <div className="card-title_block">
+                            <Image
+                                id="member-image1"
+                                className="member-image"
+                                src="/images/placeholder.png"
+                                alt="Member Image 1"
+                                width={64}  
+                                height={64}  
+                            />
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle_text">Category #1e</h3>
+                                <h2 className="card-title_text">Amazon</h2>
+                            </div>
+                        </div>
+                        <div className="card-divider"></div>
+                        <div className="card-text_container">
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle2_text">What have we listed?</h3>
+                                <p className="card-body_text">
+                                    Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
+                                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="button-wrapper">
+                        <Link href="/" passHref>
+                            <button className="secondary-action">Review List</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="member-card">
+                    <div className="upper-card_row">
+                        <div className="card-title_block">
+                            <Image
+                                id="member-image1"
+                                className="member-image"
+                                src="/images/placeholder.png"
+                                alt="Member Image 1"
+                                width={64}  
+                                height={64}  
+                            />
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle_text">Category #1e</h3>
+                                <h2 className="card-title_text">Amazon</h2>
+                            </div>
+                        </div>
+                        <div className="card-divider"></div>
+                        <div className="card-text_container">
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle2_text">What have we listed?</h3>
+                                <p className="card-body_text">
+                                    Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
+                                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="button-wrapper">
+                        <Link href="/" passHref>
+                            <button className="secondary-action">Review List</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="member-card">
+                    <div className="upper-card_row">
+                        <div className="card-title_block">
+                            <Image
+                                id="member-image1"
+                                className="member-image"
+                                src="/images/placeholder.png"
+                                alt="Member Image 1"
+                                width={64}  
+                                height={64}  
+                            />
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle_text">Category #1e</h3>
+                                <h2 className="card-title_text">Amazon</h2>
+                            </div>
+                        </div>
+                        <div className="card-divider"></div>
+                        <div className="card-text_container">
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle2_text">What have we listed?</h3>
+                                <p className="card-body_text">
+                                    Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
+                                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="button-wrapper">
+                        <Link href="/" passHref>
+                            <button className="secondary-action">Review List</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="member-card">
+                    <div className="upper-card_row">
+                        <div className="card-title_block">
+                            <Image
+                                id="member-image1"
+                                className="member-image"
+                                src="/images/placeholder.png"
+                                alt="Member Image 1"
+                                width={64}  
+                                height={64}  
+                            />
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle_text">Category #1e</h3>
+                                <h2 className="card-title_text">Amazon</h2>
+                            </div>
+                        </div>
+                        <div className="card-divider"></div>
+                        <div className="card-text_container">
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle2_text">What have we listed?</h3>
+                                <p className="card-body_text">
+                                    Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
+                                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="button-wrapper">
+                        <Link href="/" passHref>
+                            <button className="secondary-action">Review List</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="member-card">
+                    <div className="upper-card_row">
+                        <div className="card-title_block">
+                            <Image
+                                id="member-image1"
+                                className="member-image"
+                                src="/images/placeholder.png"
+                                alt="Member Image 1"
+                                width={64}  
+                                height={64}  
+                            />
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle_text">Category #1e</h3>
+                                <h2 className="card-title_text">Amazon</h2>
+                            </div>
+                        </div>
+                        <div className="card-divider"></div>
+                        <div className="card-text_container">
+                            <div className="card-text_block">
+                                <h3 className="card-subtitle2_text">What have we listed?</h3>
+                                <p className="card-body_text">
+                                    Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
+                                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="button-wrapper">
+                        <Link href="/" passHref>
+                            <button className="secondary-action">Review List</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
