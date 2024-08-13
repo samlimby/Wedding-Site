6bc1a1268e1935f71b420d6d365baffc30edbc59
samlimby <129619263+samlimby@users.noreply.github.com>
2024-08-13 18:13:20 +0100

    Styling Fix

--- app/registry/page.js
+++ app/registry/page.js
@@ -9,170 +9,170 @@ export default function Registry() {
     return (
       <div>
         <Header />
-        <div className="party-body">
-            <div className="grid-block">
-                <div className="member-card">
-                    <div className="upper-card_row">
-                        <div className="card-title_block">
+        <div className="re-party-body">
+            <div className="re-grid-block">
+                <div className="re-member-card">
+                    <div className="re-upper-card_row">
+                        <div className="re-card-title_block">
                             <Image
                                 id="member-image1"
-                                className="member-image"
+                                className="re-member-image"
                                 src="/images/placeholder.png"
                                 alt="Member Image 1"
                                 width={64}  
                                 height={64}  
                             />
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle_text">Category #1e</h3>
-                                <h2 className="card-title_text">Amazon</h2>
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle_text">Category #1e</h3>
+                                <h2 className="re-card-title_text">Amazon</h2>
                             </div>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_container">
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle2_text">What have we listed?</h3>
-                                <p className="card-body_text">
+                        <div className="re-card-divider"></div>
+                        <div className="re-card-text_container">
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle2_text">What have we listed?</h3>
+                                <p className="re-card-body_text">
                                     Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="button-wrapper">
+                    <div className="re-button-wrapper">
                         <Link href="/" passHref>
-                            <button className="secondary-action">Review List</button>
+                            <button className="re-secondary-action">Review List</button>
                         </Link>
                     </div>
                 </div>
-                <div className="member-card">
-                    <div className="upper-card_row">
-                        <div className="card-title_block">
+                <div className="re-member-card">
+                    <div className="re-upper-card_row">
+                        <div className="re-card-title_block">
                             <Image
                                 id="member-image1"
-                                className="member-image"
+                                className="re-member-image"
                                 src="/images/placeholder.png"
                                 alt="Member Image 1"
                                 width={64}  
                                 height={64}  
                             />
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle_text">Category #1e</h3>
-                                <h2 className="card-title_text">Amazon</h2>
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle_text">Category #1e</h3>
+                                <h2 className="re-card-title_text">Amazon</h2>
                             </div>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_container">
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle2_text">What have we listed?</h3>
-                                <p className="card-body_text">
+                        <div className="re-card-divider"></div>
+                        <div className="re-card-text_container">
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle2_text">What have we listed?</h3>
+                                <p className="re-card-body_text">
                                     Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="button-wrapper">
+                    <div className="re-button-wrapper">
                         <Link href="/" passHref>
-                            <button className="secondary-action">Review List</button>
+                            <button className="re-secondary-action">Review List</button>
                         </Link>
                     </div>
                 </div>
-                <div className="member-card">
-                    <div className="upper-card_row">
-                        <div className="card-title_block">
+                <div className="re-member-card">
+                    <div className="re-upper-card_row">
+                        <div className="re-card-title_block">
                             <Image
                                 id="member-image1"
-                                className="member-image"
+                                className="re-member-image"
                                 src="/images/placeholder.png"
                                 alt="Member Image 1"
                                 width={64}  
                                 height={64}  
                             />
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle_text">Category #1e</h3>
-                                <h2 className="card-title_text">Amazon</h2>
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle_text">Category #1e</h3>
+                                <h2 className="re-card-title_text">Amazon</h2>
                             </div>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_container">
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle2_text">What have we listed?</h3>
-                                <p className="card-body_text">
+                        <div className="re-card-divider"></div>
+                        <div className="re-card-text_container">
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle2_text">What have we listed?</h3>
+                                <p className="re-card-body_text">
                                     Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="button-wrapper">
+                    <div className="re-button-wrapper">
                         <Link href="/" passHref>
-                            <button className="secondary-action">Review List</button>
+                            <button className="re-secondary-action">Review List</button>
                         </Link>
                     </div>
                 </div>
-                <div className="member-card">
-                    <div className="upper-card_row">
-                        <div className="card-title_block">
+                <div className="re-member-card">
+                    <div className="re-upper-card_row">
+                        <div className="re-card-title_block">
                             <Image
                                 id="member-image1"
-                                className="member-image"
+                                className="re-member-image"
                                 src="/images/placeholder.png"
                                 alt="Member Image 1"
                                 width={64}  
                                 height={64}  
                             />
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle_text">Category #1e</h3>
-                                <h2 className="card-title_text">Amazon</h2>
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle_text">Category #1e</h3>
+                                <h2 className="re-card-title_text">Amazon</h2>
                             </div>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_container">
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle2_text">What have we listed?</h3>
-                                <p className="card-body_text">
+                        <div className="re-card-divider"></div>
+                        <div className="re-card-text_container">
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle2_text">What have we listed?</h3>
+                                <p className="re-card-body_text">
                                     Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="button-wrapper">
+                    <div className="re-button-wrapper">
                         <Link href="/" passHref>
-                            <button className="secondary-action">Review List</button>
+                            <button className="re-secondary-action">Review List</button>
                         </Link>
                     </div>
                 </div>
-                <div className="member-card">
-                    <div className="upper-card_row">
-                        <div className="card-title_block">
+                <div className="re-member-card">
+                    <div className="re-upper-card_row">
+                        <div className="re-card-title_block">
                             <Image
                                 id="member-image1"
-                                className="member-image"
+                                className="re-member-image"
                                 src="/images/placeholder.png"
                                 alt="Member Image 1"
                                 width={64}  
                                 height={64}  
                             />
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle_text">Category #1e</h3>
-                                <h2 className="card-title_text">Amazon</h2>
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle_text">Category #1e</h3>
+                                <h2 className="re-card-title_text">Amazon</h2>
                             </div>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_container">
-                            <div className="card-text_block">
-                                <h3 className="card-subtitle2_text">What have we listed?</h3>
-                                <p className="card-body_text">
+                        <div className="re-card-divider"></div>
+                        <div className="re-card-text_container">
+                            <div className="re-card-text_block">
+                                <h3 className="re-card-subtitle2_text">What have we listed?</h3>
+                                <p className="re-card-body_text">
                                     Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="button-wrapper">
+                    <div className="re-button-wrapper">
                         <Link href="/" passHref>
-                            <button className="secondary-action">Review List</button>
+                            <button className="re-secondary-action">Review List</button>
                         </Link>
                     </div>
                 </div>
--- app/wedding_party/page.js
+++ app/wedding_party/page.js
@@ -8,122 +8,122 @@ export default function WeddingParty() {
     return (
       <div>
         <Header />
-        <div className="party-body">
-            <div className="grid-block">
-                <div className="member-card">
+        <div className="wp-party-body">
+            <div className="wp-grid-block">
+                <div className="wp-member-card">
                     <Image
                         id="member-image1"
-                        className="member-image"
+                        className="wp-member-image"
                         src="/images/placeholder.png"
                         alt="Member Image 1"
                         width={240}  
                         height={200}  
                     />
-                    <div className="card-text_container">
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle_text">Bride</h3>
-                            <h2 className="card-title_text">Meg</h2>
+                    <div className="wp-card-text_container">
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle_text">Bride</h3>
+                            <h2 className="wp-card-title_text">Meg</h2>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle2_text">Fun Fact</h3>
-                            <p className="card-body_text">
+                        <div className="wp-card-divider"></div>
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle2_text">Fun Fact</h3>
+                            <p className="wp-card-body_text">
                                 Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className="member-card">
+                <div className="wp-member-card">
                     <Image
                         id="member-image1"
-                        className="member-image"
+                        className="wp-member-image"
                         src="/images/placeholder.png"
                         alt="Member Image 1"
                         width={240}  
                         height={200}  
                     />
-                    <div className="card-text_container">
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle_text">Bride</h3>
-                            <h2 className="card-title_text">Meg</h2>
+                    <div className="wp-card-text_container">
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle_text">Bride</h3>
+                            <h2 className="wp-card-title_text">Meg</h2>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle2_text">Fun Fact</h3>
-                            <p className="card-body_text">
+                        <div className="wp-card-divider"></div>
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle2_text">Fun Fact</h3>
+                            <p className="wp-card-body_text">
                                 Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className="member-card">
+                <div className="wp-member-card">
                     <Image
                         id="member-image1"
-                        className="member-image"
+                        className="wp-member-image"
                         src="/images/placeholder.png"
                         alt="Member Image 1"
                         width={240}  
                         height={200}  
                     />
-                    <div className="card-text_container">
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle_text">Bride</h3>
-                            <h2 className="card-title_text">Meg</h2>
+                    <div className="wp-card-text_container">
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle_text">Bride</h3>
+                            <h2 className="wp-card-title_text">Meg</h2>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle2_text">Fun Fact</h3>
-                            <p className="card-body_text">
+                        <div className="wp-card-divider"></div>
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle2_text">Fun Fact</h3>
+                            <p className="wp-card-body_text">
                                 Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className="member-card">
+                <div className="wp-member-card">
                     <Image
                         id="member-image1"
-                        className="member-image"
+                        className="wp-member-image"
                         src="/images/placeholder.png"
                         alt="Member Image 1"
                         width={240}  
                         height={200}  
                     />
-                    <div className="card-text_container">
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle_text">Bride</h3>
-                            <h2 className="card-title_text">Meg</h2>
+                    <div className="wp-card-text_container">
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle_text">Bride</h3>
+                            <h2 className="wp-card-title_text">Meg</h2>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle2_text">Fun Fact</h3>
-                            <p className="card-body_text">
+                        <div className="wp-card-divider"></div>
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle2_text">Fun Fact</h3>
+                            <p className="wp-card-body_text">
                                 Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className="member-card">
+                <div className="wp-member-card">
                     <Image
                         id="member-image1"
-                        className="member-image"
+                        className="wp-member-image"
                         src="/images/placeholder.png"
                         alt="Member Image 1"
                         width={240}  
                         height={200}  
                     />
-                    <div className="card-text_container">
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle_text">Bride</h3>
-                            <h2 className="card-title_text">Meg</h2>
+                    <div className="wp-card-text_container">
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle_text">Bride</h3>
+                            <h2 className="wp-card-title_text">Meg</h2>
                         </div>
-                        <div className="card-divider"></div>
-                        <div className="card-text_block">
-                            <h3 className="card-subtitle2_text">Fun Fact</h3>
-                            <p className="card-body_text">
+                        <div className="wp-card-divider"></div>
+                        <div className="wp-card-text_block">
+                            <h3 className="wp-card-subtitle2_text">Fun Fact</h3>
+                            <p className="wp-card-body_text">
                                 Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. 
                                 Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
                             </p>
